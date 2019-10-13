5fb0153c665d1c22a7c96762afcfb69a4a16b358
Lily <lili_sakura@126.com>
2019-10-13 20:58:22 +0800

    get bash permission

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,20 +3,19 @@
 # 终止一个错误
 set -e
 
-# git pull
 # 构建
-# npm run build
+npm run build
 
 # 进入生成的构建文件夹
-# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果你是要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
-# git add -A
-# git commit -m 'deploy'
+git add -A
+git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:sakuraLili/sakuralili.github.io.git release
+git push -f git@github.com:sakuraLili/sakuralili.github.io.git release
 
 cd -
